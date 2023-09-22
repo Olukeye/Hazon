918beca30fbf2b1230ea7e3474ca49ceb2f474fc
Sunkanmi Olukeye <53058618+Olukeye@users.noreply.github.com>
2023-09-22 21:57:46 +0100

    Update adminRoute.js

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,8 +1,1 @@
-const express = require("express");
 
-const router = express.Router();
-const donorRoute = require("../controllers/donorController");
-
-router.route("/").get(donorRoute.getAllDonors);
-
-module.exports = router;
