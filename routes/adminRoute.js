a9fcbd1dfd6d29d556ffbf47b3fb540be369a835
Sunkanmi Olukeye <53058618+Olukeye@users.noreply.github.com>
2023-09-22 15:50:12 +0100

    hello

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 const donorRoute = require("../controllers/donorController");
-const { authenticateUser, authorize } = require("../middleware/authentication");
 
 router.route("/").get(donorRoute.getAllDonors);
 
